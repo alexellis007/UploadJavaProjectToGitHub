34f4e89d0a30d0241048c9a8dd90295fc96a03f1
Alex Ellis <aellis007@gmail.com>
2018-12-11 13:40:49 -0600

    Add 'Commit to remote repository' message

--- src/com/forteknowledge/Main.java
+++ src/com/forteknowledge/Main.java
@@ -3,6 +3,6 @@ package com.forteknowledge;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Commit to remote repository");
     }
 }
